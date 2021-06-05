84c87d31dd2e891ec584bb45bbacffbc87168b36
evgeniyGusev <shchelkalinevgeniy@gmail..com>
2021-06-05 23:01:31 +0300

    new commit

---
+++ src/components/general/AppContainer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app-container">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppContainer',
+};
+</script>
+
+<style lang="scss" scoped>
+.app-container {
+  max-width: 955px;
+  margin: 0 auto;
+  padding: 0 20px;
+}
+</style>
---
+++ src/layouts/components/defaultLayout/TheHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="the-header">
+    HEADER
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+};
+</script>
+
+<style lang="scss" scoped>
+.the-header {
+  padding: 40px 0;
+  background-color: forestgreen;
+  text-align: center;
+  color: #fff;
+}
+</style>
